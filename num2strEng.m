f4a640e84b7a68f867fa3317e523dee8f405117d
shyamalnz <prasadshya@myvuw.ac.nz>
2017-02-22 01:40:03 +1300

    Cannot remember reason for changes.

--- num2strEng.m
+++ num2strEng.m
@@ -11,20 +11,42 @@ function xstr=num2strEng(x,varargin)
 
 prefices={'Y','Z','E','P','T','G','M','k','','m','\mu','n','p','f','a','z','y'};
 
-x=x(:);
-logbase=3*floor(log10(x)/3);
+
+%x=x(:);
+logx=log10(x);
+logbase=3*floor(logx/3);
 
 inrange=(logbase>=-24)&(logbase<=24);
 logbase(~inrange)=0;
 
 ibase=1-(logbase-24)/3; %
 
-if length(varargin)==1,
+if ~isempty(varargin),
     n=varargin{1};
 else
     n=-1;
 end
-xstr=[feval('num2str',x./10.^logbase,n) repmat(' ',length(x),1) strvcat(prefices{ibase})];
 
+if length(varargin) > 1
+    prefices{11} = 'u';
+end
 
+%xstr=[feval('num2str',x./10.^logbase,n) repmat(' ',length(x),1) strvcat(prefices{ibase})];
+if size(x,2) > 1
+    xstr=strcat(strtrim(cellstr(num2str(x(:,1)./10.^max(logbase,[],2),n))),'-',strtrim(cellstr(num2str(x(:,2)./10.^max(logbase,[],2),n))),strcat({' '},prefices(min(ibase,[],2))'));
+else
+    if length(varargin) > 1
+        
+    xstr=strcat(num2str(x./10.^logbase,n),strcat({' '},prefices(ibase)'));
+    else
+        xstr=strtrim(strcat(num2str(x./10.^logbase,n),strcat({' '},prefices(ibase)')));
+    end
+end
+
+if length(xstr) == 1
+    xstr = xstr{1};
+end
+if  isempty(xstr)
+    xstr = '';
+end
 
