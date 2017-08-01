d096aac65980ce163fa8714988705e99f83cb551
shyamalnz <prasadshya@myvuw.ac.nz>
2017-08-01 13:29:24 +1200

    percential and data number options

--- f_zLim.m
+++ f_zLim.m
@@ -7,10 +7,27 @@ opt.nth = 10;
 % user input
 [opt] = f_OptSet(opt, varargin);
 
-data = sort(data,1);
-if size(data,1) > opt.nth
-    zLim = [min(data(opt.nth,:)), max(data(end-opt.nth+1,:))*1.2];
+data = sort(data(:),1);
+
+if opt.nth < 1
+    
+    pct = opt.nth;
+    if pct > 0.5
+       pct = 1 - pct; 
+    end
+    
+    zLim = [prctile(data,pct*100),prctile(data,(1-pct)*100)];
+    
+    if zLim(2) < 0, zLim(2) = zLim(2)*1.2; else zLim(2) = zLim(2)/1.2; end
     if zLim(1) < 0, zLim(1) = zLim(1)*1.2; else zLim(1) = zLim(1)/1.2; end
+
+elseif size(data,1) > opt.nth
+    
+    
+    zLim = [min(data(opt.nth,:)), max(data(end-opt.nth+1,:))*1.2];
+    
+    if zLim(1) < 0, zLim(1) = zLim(1)*1.2; else zLim(1) = zLim(1)/1.2; end
+    
 else
     zLim = [min(data),max(data)];
 end
